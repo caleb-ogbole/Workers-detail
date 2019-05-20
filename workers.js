3b5048986947e1586f38238b0a698b028e4852c9
caleb-ogbole <47568685+caleb-ogbole@users.noreply.github.com>
2019-05-20 15:34:56 +0100

    Add files via upload

---
+++ workers.js
@@ -0,0 +1,495 @@
+const workers= [
+    {
+        FirstName: "Vanessa",
+        LastName: "Rogers",
+        Gender: "Female",
+        Age: 19,	
+        Salary: 60193,
+    },
+    {
+           FirstName: "Charlotte",
+           LastName: "Hunt",
+           Gender: "Female",
+           Age: 24,	
+           Salary: 92383,
+
+    } ,
+    {
+        FirstName: "Marcus",
+        LastName: "Foster",
+        Gender: "Male",
+        Age: 21,	
+        Salary: 65760,
+    },
+    {
+        FirstName: "Myra",
+        LastName: "Brooks",
+        Gender: "Male",
+        Age: 21,	
+        Salary: 174036,
+    },
+    {
+        FirstName: "Caroline",
+        LastName:"Armstrong",
+        Gender: "Female",
+        Age: 29,	
+        Salary: 149879,
+       },
+       {
+        FirstName: "Belinda",
+        LastName:"Carroll",
+        Gender: "Female",
+        Age: 19,	
+        Salary: 188935,
+       },
+       {
+        FirstName: "Olivia",
+        LastName:"Armstrong",
+        Gender: "Female",
+        Age: 26,	
+        Salary: 34782,
+       },
+       {
+        FirstName: "April",
+        LastName:"Cooper",
+        Gender: "Female",
+        Age: 20,	
+        Salary: 68263,
+       },
+       {
+        FirstName: "Edith",
+        LastName:"Harrison",
+        Gender: "Male",
+        Age: 23,	
+        Salary: 54709
+       },
+       {
+        FirstName: "Vivian",
+        LastName:"Henderson",
+        Gender: "Female",
+        Age: 20,	
+        Salary: 155709
+       },
+       {
+        FirstName: "Ashton",
+        LastName:"Martin",
+        Gender: "Male",
+        Age: 27,	
+        Salary: 166647,
+       },
+       {
+        FirstName: "Gianna",
+        LastName:"Moore",
+        Gender: "Female",
+        Age: 23,	
+        Salary: 191619,
+       },
+       {
+        FirstName: "Camila",
+        LastName:"Allen",
+        Gender: "Female",
+        Age: 23,	
+        Salary: 85567,
+       },
+{
+    FirstName: "Gianna",
+    LastName:"Morrison",
+    Gender: "Female",
+    Age: 28,	
+    Salary: 191509
+},
+{
+    FirstName: "Paul",
+    LastName:"Cooper",
+    Gender: "Male",
+    Age: 28,	
+    Salary: 39471,
+},
+{
+    FirstName: "Tara",
+    LastName:"Mason",
+    Gender: "Female",
+    Age: 19,	
+    Salary: 189780,
+},
+{
+    FirstName: "Aida",
+    LastName:"Tucker",
+    Gender: "Female",
+    Age: 21,	
+    Salary: 143187,
+},
+{
+    FirstName: "Richard",
+    LastName:"Cameron",
+    Gender: "Male",
+    Age: 21,	
+    Salary: 116644,
+},
+{
+    FirstName: "Amy",
+    LastName:"Stewart",
+    Gender: "Male",
+    Age: 29,	
+    Salary: 126961,
+},
+{
+    FirstName: "Eleanor",
+    LastName:"Thompson",
+    Gender: "Female",
+    Age: 22,	
+    Salary: 159003,
+},
+{
+    FirstName: "Alberta",
+    LastName:"Edwards",
+    Gender: "Male",
+    Age: 21,	
+    Salary: 105648
+},
+{
+    FirstName: "Gianna",
+    LastName:"Evans",
+    Gender: "Female",
+    Age: 27,	
+    Salary: 183805,
+},
+{
+    FirstName: "Florrie",
+    LastName:"Kelley",
+    Gender: "Female",
+    Age: 24,	
+    Salary: 180568,
+},
+{
+    
+    FirstName: "Miranda",
+    LastName:"Harris",
+    Gender: "Female",
+    Age: 19,	
+    Salary: 185753,
+},
+{
+    FirstName: "Henry",
+    LastName:"Davis",
+    Gender: "Male",
+    Age: 18,	
+    Salary: 83804
+},
+{
+    FirstName: "Kirsten",
+    LastName:"Barnes",
+    Gender: "Male",
+    Age: 26,	
+    Salary: 161833,
+},
+{
+    FirstName: "Jack",
+    LastName:"Chapman",
+    Gender: "Male",
+    Age:19,
+    Salary: 41007
+},
+{
+    FirstName: "Myra",
+    LastName:"Kelly",
+    Gender: "Female",
+    Age: 27,	
+    Salary: 118346,
+},
+{
+    FirstName: "Emma",
+    LastName:"Evans",
+    Gender: "Female",
+    Age: 23,	
+    Salary: 126125,
+},
+{
+    FirstName: "Blake",
+    LastName:"Evans",
+    Gender: "Male",
+    Age: 24,	
+    Salary: 69702,
+},
+{
+    
+    FirstName: "Aldus",
+    LastName:"Armstrong",
+    Gender: "Male",
+    Age: 26,	
+    Salary: 132491,
+},
+{
+    FirstName: "Adelaide",
+    LastName:"Martin",
+    Gender: "Female",
+    Age: 20,	
+    Salary: 175263,
+},
+{
+    FirstName: "Victoria",
+    LastName:"Elliott",
+    Gender: "Female",
+    Age: 21,	
+    Salary: 111645,
+},
+{
+    FirstName: "Nicole",
+    LastName:"Harrison",
+    Gender: "Female",
+    Age: 22,	
+    Salary: 54624,
+},
+{
+    FirstName: "Edwin",
+    LastName:"Hamilton",
+    Gender: "Male",
+    Age: 18,	
+    Salary: 152586,
+},
+{
+    FirstName: "Jessica",
+    LastName:"Robinson",
+    Gender: "Female",
+    Age: 29,	
+    Salary: 85446,
+},
+{
+    FirstName: "Sofia",
+    LastName:"Turner",
+    Gender: "Female",
+    Age: 20,	
+    Salary: 131564,
+},
+{
+    FirstName: "Cadie",
+    LastName:"Higgins",
+    Gender: "Male",
+    Age: 21,	
+    Salary: 138663,
+},
+{
+    FirstName: "Jessica",
+    LastName:"Turner",
+    Gender: "Female",
+    Age: 24,	
+    Salary: 91319
+},
+{
+    FirstName: "Walter",
+    LastName:"Hamilton",
+    Gender: "Male",
+    Age: 25,	
+    Salary: 117408,
+},
+{
+    FirstName: "Alexander",
+    LastName:"Farrell",
+    Gender: "Male",
+    Age: 26,	
+    Salary: 65878
+},
+{
+    FirstName: "Justin",
+    LastName:"Howard",
+    Gender: "Male",
+    Age: 21,	
+    Salary: 165575,
+},
+{
+    FirstName: "Chelsea",
+    LastName:"Holmes",
+    Gender: "Female",
+    Age: 21,	
+    Salary: 32690,
+},
+{
+    FirstName: "Alberta",
+    LastName:"Wilson",
+    Gender: "Male",
+    Age: 22,	
+    Salary: 143459,
+},
+{
+    FirstName: "Miller",
+    LastName:"Murphy",
+    Gender: "Male",
+    Age: 25,	
+    Salary: 163776,
+},
+{
+    FirstName: "Adrianna",
+    LastName:"Hawkins",
+    Gender: "Female",
+    Age: 29,	
+    Salary: 64412,
+},
+{
+    FirstName: "Dominik",
+    LastName:"Reed",
+    Gender: "Male",
+    Age: 23,
+    Salary: 42643,
+},
+{
+    FirstName: "Gianna",
+    LastName:"Craig",
+    Gender: "Female",
+    Age: 18,	
+    Salary: 141944,
+},
+{
+    FirstName: "Alissa",
+    LastName:"Cameron",
+    Gender: "Female",
+    Age: 25,	
+    Salary: 101581,
+},
+{
+    FirstName: "Aldus",
+    LastName:"Adams",
+    Gender: "Female",
+    Age:28,
+    Salary:132894,
+},
+];
+//first and last names of all workers
+console.log( " First and last name of all workers". toUpperCase( ));
+for(let i=0; i < workers.length; i++ )
+{
+    console.log(workers[i].FirstName + " " + workers[i].LastName);
+}; 
+//Total Salary
+console.log(" Total Salary of all workers".toUpperCase());
+let totalSalary = 0;
+for ( let b = 0; b < workers.length; b++) {
+    totalSalary +=workers[b].salary
+};
+console.log(totalSalary);
+//average age of workers
+let averageAge = 0;
+for(let c = 0; c < workers.length; c++ ) {
+    averageAge+= workers[c].age/workers.length
+}
+console.log("average age of all workers".toUpperCase());
+console.log(averageAge);
+//personal info of worker index 33
+console.log("Worker at index 33".toUpperCase());
+console.log("FIRST NAME: " + " "+workers[33].FirstName);
+console.log("LAST NAME: " + " "+workers[33].LastName);
+console.log("SALARY: " + " "+workers[33].Salary);
+console.log("AGE:" + " "+workers[33].Age);
+console.log("GENDER: " + " "+workers[33].Gender);
+//salary of sofia turner
+console.log("salary of sofia turner".toUpperCase());
+console.log(workers[36].salary);
+//length of array
+console.log("length of arrays".toUpperCase());
+console.log(workers.length)
+//correction of details
+console.log("edited name of worker".toUpperCase());
+workers[44].FirstName ="Milner";
+let fullName = workers[44].FirstName + " "+ workers[44].LastName;
+console.log(fullName); 
+//updated arrays
+console.log("Updated array".toUpperCase());
+console.log(workers);
+//Gender
+console.log("Editing gender of worker".toUpperCase());
+workers[49].Gender = "Female";
+console.log(workers[49].Gender);
+console.log(workers);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+       
+        
+       
+       
+       
+      
+        
+        
+       
+     
+        
+       
+       
+        
+        
+       
+        
+       
+        
+        
+        
+       
+        
+       
+        
+      
+        
+        
+        
+        
+       
+        
+       
+      
+      
+        
+       
+       
+        
+        
+       
+    
+        
+       
+       
+ 
+ 
